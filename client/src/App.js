c27c9c52a7a9ebd96296e154de695cace0f66549
Brendan <53023770+brendankhoury@users.noreply.github.com>
2020-09-25 22:45:00 -0700

    Added theme support and an AppBar to the top of the site

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import engine from './personalization_engine/engine.js'
+import { createMuiTheme } from '@material-ui/core/styles';
+import { purple, deepPurple } from '@material-ui/core/colors';
+import { ThemeProvider } from '@material-ui/styles';
+import { AppBar, IconButton, Toolbar, Typography, Button } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+const theme = createMuiTheme({
+  palette: {
+    primary: deepPurple,
+  },
+});
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    var feed = new engine();
+    var text = JSON.stringify(feed.request_feed());
+    return (
+      <ThemeProvider theme={theme}>
+        <AppBar align='center' position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" > 
+              Padlock News
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
