68238e9eb386f873c25e1d756e4660a1226a651c
Brendan <53023770+brendankhoury@users.noreply.github.com>
2020-10-03 23:58:01 -0700

    cleaned up the way that the non-main articles are displayed, used a map call instead of manually writing each card

--- client/src/components/SubjectSection.jsx
+++ client/src/components/SubjectSection.jsx
@@ -13,7 +13,7 @@ const useStyles = makeStyles((theme) => ({
 export default function SubjectSection(props) {
     // Props should have 4 articles in order of importance,
     // The top article will be displayed on a big card and the rest on small cards.
-    // console.log(props)
+    console.log(props)
     const classes = useStyles()
     return (
         <div className={classes.root}>
@@ -31,7 +31,6 @@ export default function SubjectSection(props) {
                 </Grid>
                 <Grid item xs={12} sm={0}>
                     <Hidden smUp>
-
                         <SmallNewsCard
                             title={props.articles[0].title} 
                             summary={props.articles[0].summary}
@@ -40,30 +39,16 @@ export default function SubjectSection(props) {
                     </Hidden>
                     {/* Article 1 (when the screen is small(xs) the small card is used) */}
                 </Grid>
-                <Grid item xs={12} sm={4}>
-                    {/* Article 2 */}
-                    <SmallNewsCard
-                        title={props.articles[1].title} 
-                        summary={props.articles[1].summary}
-                        image={props.articles[1].imageURL}
-                        />
-                </Grid>
-                <Grid item xs={12} sm={4} >
-                    {/* Article 3 */}
-                    <SmallNewsCard
-                        title={props.articles[2].title} 
-                        summary={props.articles[2].summary}
-                        image={props.articles[2].imageURL}
-                        />
-                </Grid>
-                <Grid item xs={12} sm={4} >
-                    {/* Article 4 */}
-                    <SmallNewsCard
-                        title={props.articles[3].title} 
-                        summary={props.articles[3].summary}
-                        image={props.articles[3].imageURL}
-                        />
-                </Grid>
+                {props.articles.slice(1).map(article =>                 
+                    <Grid item xs={12} sm={4}>
+                        <SmallNewsCard
+                            title={article.title} 
+                            summary={article.summary}
+                            image={article.imageURL}
+                            />
+                    </Grid>)
+                }    
+
             </Grid>
         </div>
         
