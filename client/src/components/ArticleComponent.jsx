84a6d0eebad118b6d8c42cc4404a218367e89475
Brendan <53023770+brendankhoury@users.noreply.github.com>
2020-10-07 23:14:09 -0700

    Enabled the article to display text from an object and map out paragraphs

--- client/src/components/ArticleComponent.jsx
+++ client/src/components/ArticleComponent.jsx
@@ -4,32 +4,32 @@ import React from 'react'
 const useStyles = makeStyles((theme) => ({
     paragraph:{
         textIndent: "50px",
+        marginTop: "10px",
     },
     article:{
         marginTop:"10px"
     }
 }));
 
+const articleTest = {
+    "title":"Quality not clickbait title",
+    "content": `Intreguing article content. Imagine an article here, any article. Something that conveys information. Something that can change someones mind, change the world, or maybe just make someones day. The posibilities are endles, and so is this short essay where I try to test the look of this article on my page. 
+    More Intreguing article content. Imagine an article here, any article. Something that conveys information. Something that can change someones mind, change the world, or maybe just make someones day. The posibilities are endles, and so is this short essay where I try to test the look of this article on my page. `
 
+}
 
 export default function ViewArticle(props) {
     //Request article content somehow
     console.log(props.location)
     const classes = useStyles();
+    // Imagine articleTest as props.article
     return (
         <Grid container>
             <Grid item xs={0}  sm={1}/>
             <Grid item xs={12} sm={10}>
                 <article className = {classes.article}>
-                    <Typography variant="h3">Quality not clickbait title</Typography>
-                    <Typography className={classes.paragraph}>
-                        {/* {props.articleContent} */}
-                        Intreguing article content. Imagine an article here, any article. Something that conveys information. Something that can change someones mind, change the world, or maybe just make someones day. The posibilities are endles, and so is this short essay where I try to test the look of this article on my page. 
-                    </Typography>  
-                    <Typography className={classes.paragraph}>
-                        {/* {props.articleContent} */}
-                        More Intreguing article content. Imagine an article here, any article. Something that conveys information. Something that can change someones mind, change the world, or maybe just make someones day. The posibilities are endles, and so is this short essay where I try to test the look of this article on my page. 
-                    </Typography> 
+                    <Typography variant="h3">{articleTest.title}</Typography>
+                    {articleTest.content.split('\n').map(paragraph => <Typography className={classes.paragraph}>{paragraph}</Typography>)}
                 </article>
             </Grid>
             <Grid item xs={0}  sm={1}/>
