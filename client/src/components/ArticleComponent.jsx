df34ca1c15eef97dccf7a00b7ec6d9f3c3147e6e
Brendan <53023770+brendankhoury@users.noreply.github.com>
2020-10-08 17:09:45 -0700

    Added author and dateWritten to the article object

--- client/src/components/ArticleComponent.jsx
+++ client/src/components/ArticleComponent.jsx
@@ -13,6 +13,8 @@ const useStyles = makeStyles((theme) => ({
 
 const articleTest = {
     "title":"Quality not clickbait title",
+    "author": "Dr. Suess",
+    "dateWritten": "10/08/20",
     "content": `Intreguing article content. Imagine an article here, any article. Something that conveys information. Something that can change someones mind, change the world, or maybe just make someones day. The posibilities are endles, and so is this short essay where I try to test the look of this article on my page. 
     More Intreguing article content. Imagine an article here, any article. Something that conveys information. Something that can change someones mind, change the world, or maybe just make someones day. The posibilities are endles, and so is this short essay where I try to test the look of this article on my page. `
 
@@ -29,6 +31,10 @@ export default function ViewArticle(props) {
             <Grid item xs={12} sm={10}>
                 <article className = {classes.article}>
                     <Typography variant="h3">{articleTest.title}</Typography>
+                    <center>
+                        <Typography >Written By: {articleTest.author}</Typography>
+                        <Typography >Written On: {articleTest.dateWritten}</Typography>
+                    </center>
                     {articleTest.content.split('\n').map(paragraph => <Typography className={classes.paragraph}>{paragraph}</Typography>)}
                 </article>
             </Grid>
