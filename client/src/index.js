e24bb61a9738699ebf87d8d5d58361573791e29b
Brendan <53023770+brendankhoury@users.noreply.github.com>
2020-10-08 20:48:50 -0700

    Added a login button

--- client/src/index.js
+++ client/src/index.js
@@ -6,16 +6,18 @@ import App from './App';
 import ArticleComponent from './components/ArticleComponent'
 import * as serviceWorker from './serviceWorker';
 import { createMuiTheme } from '@material-ui/core/styles';
-import { deepPurple } from '@material-ui/core/colors';
+import { deepPurple, lightBlue } from '@material-ui/core/colors';
 import { ThemeProvider } from '@material-ui/styles';
-import { AppBar, IconButton, Toolbar, Typography} from '@material-ui/core';
+import { AppBar, Button, IconButton, Toolbar, Typography} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
 const theme = createMuiTheme({
   palette: {
     primary: deepPurple,
+    secondary: lightBlue
   },
 });
+
 
 const routing = (
   <Router>
@@ -25,9 +27,10 @@ const routing = (
             <IconButton edge="start" color="inherit" aria-label="menu">
               <MenuIcon />
             </IconButton>
-            <Typography variant="h6" > 
+            <Typography variant="h6" className="title"> 
               Padlock News
             </Typography>
+            <Button variant="contained" color="secondary">Login</Button>
           </Toolbar>
         </AppBar>
         <Route exact path="/" component={App} />
