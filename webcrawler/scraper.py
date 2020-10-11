a87299ca113ea840b941bfb7aa1629bf887512f8
Phillip Kopita <kopitp@rpi.edu>
2020-10-11 16:21:28 -0400

    Swtiched to using argparse for CLI args

--- webcrawler/scraper.py
+++ webcrawler/scraper.py
@@ -4,28 +4,27 @@ from newsplease import NewsPlease 	# For parsing articles from homepage of websi
 import urllib.parse
 from urllib.error import HTTPError
 import argparse
-import sys # DEBUG (replace with argparse)
 
 if __name__ == "__main__":
 
-	# Process commandline arguments
-	#parser = argparse.ArgumentParser(description='Scrape articles from a given news site')
-	#parser.add_argument
-	username = sys.argv[1]
-	password = sys.argv[2]
-	site = sys.argv[3]
+	# Process command-line arguments
+	parser = argparse.ArgumentParser(prog='scraper.py', description='Scrape articles from a given news site')
+	parser.add_argument('username', help='Username for MongoDB database access')
+	parser.add_argument('password', help='Password for MongoDB database access')
+	parser.add_argument('site_url', help='URL to news site homepage for scraping')
+	args = parser.parse_args()
 
 	# Connect to MongoDB database
-	username = urllib.parse.quote_plus(username)
-	password = urllib.parse.quote_plus(password)
-	database = urllib.parse.quote_plus('newssite_test')
+	username = urllib.parse.quote_plus(args.username)
+	password = urllib.parse.quote_plus(args.password)
+	database = urllib.parse.quote_plus('newssite_test') # DEBUG (testing database)
 
 	client = pymongo.MongoClient("mongodb+srv://%s:%s@newscluster.po4ut.mongodb.net/%s?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE" % (username, password, database))
 	db = client.newssite_test
 	col = db.articles
 
 	# Scrape data from random news site
-	homepage = newspaper.build(site, memoize_articles=True)	# DEBUG (set to true to avoid duplicates)
+	homepage = newspaper.build(args.site_url, memoize_articles=True)	# DEBUG (set to true to avoid duplicates)
 
 	# Download and parse articles
 	parsed_articles = []
@@ -40,5 +39,5 @@ if __name__ == "__main__":
 
 	# Insert articles into database
 	if (parsed_articles):
-		print("Inserting...") #DEBUG
+		print("Inserting...") # DEBUG
 		inserted = col.insert_many(parsed_articles)
