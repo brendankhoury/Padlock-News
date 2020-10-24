f4571dad17d87d1ba72269c5741d7f8d3047d7d6
unknown <xuzhengwen23@gmail.com>
2020-10-24 22:59:11 +0800

    Created routes but couldn't connect to mongodb and get data

--- server/index.js
+++ server/index.js
@@ -4,23 +4,34 @@ const {MongoClient} = require('mongodb');
 const bodyParser = require('body-parser');
 require('dotenv/config');
 
+
 app.use(express.json());
 app.use(bodyParser.json());
 
-// import routes
-const articlesRt = require('./routes/articles'); 
 
-app.use('./articles', articlesRt); 
-
-
-// connect to mongodb and call functions to return articles
+// connect to mongodb
 async function main() {
   const uri = process.env.CONNECT_TO_DB;
   const client = new MongoClient(uri, { useNewUrlParser: true}, { useUnifiedTopology: true });
   try {
     await client.connect();
-    await returnAll(client);
-    await returnRecent(client);
+    app.get('/', (req, res) => {
+      res.send('homepage');
+    });
+    // return recent five articles
+    app.get('/api/recentarticles', (req, res) => {
+      const sortedArticles = client.db("newssite_test").collection("articles")
+      .find().sort({ date_publish: -1});
+      const results = sortedArticles.toArray();
+      for (var count = 0; count < results.length && count < 5; count++) {
+        res.send(results[count]);
+      }
+    });
+    // return a specific article given the id
+    app.get('/api/article/:id', (req, res) => {
+      const oneArticle = client.db("newssite_test").collection("articles").findOne({'_id':req.params.id});
+      res.send(oneArticle);
+    });
   } catch (e) {
     console.error(e);
   } finally {
@@ -30,27 +41,10 @@ async function main() {
 
 main().catch(console.err);
 
-// return the most recent articles
-async function returnRecent(client) {
-  const sortedArticles = await client.db("newssite_test").collection("articles")
-    .find().sort({ date_publish: -1});
-  const results = await sortedArticles.toArray();
-  console.log("The most recent article is: ");
-  console.log(results[0]);
-}
-
-
-// return all the articles
-async function returnAll(client) {
-  const allArticles = await client.db("newssite_test").collection("articles").find();
-  const results = await allArticles.toArray();
-  console.log("All the articles: ");
-  results.forEach(result => {
-    console.log(result);
-  });
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}...`);
 });
+
