6accf5035c6e035913fe604acb713811ab7fddaf
Brendan <53023770+brendankhoury@users.noreply.github.com>
2020-10-31 23:19:05 -0700

    Made the backend work with cross origin requests.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const {MongoClient} = require('mongodb');
 const bodyParser = require('body-parser');
@@ -8,7 +9,14 @@ require('dotenv/config');
 
 app.use(express.json());
 app.use(bodyParser.json());
-    
+
+// Ads cors functionality to the application
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.get('/', (req, res) => {
   res.send('homepage');
