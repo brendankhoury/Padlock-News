18f979d469b5900e19172cc569000197112a849f
Brendan <53023770+brendankhoury@users.noreply.github.com>
2020-10-31 23:17:20 -0700

    Got it working with the backend a little, its a start!

--- client/src/components/FeedComponent.jsx
+++ client/src/components/FeedComponent.jsx
@@ -1,29 +1,56 @@
 import React, { Component } from 'react'
-import { Grid } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import SubjectSection from './SubjectSection';
 
 import '../assets/FeedComponent.css'
 
 import engine from '../personalization_engine/engine.js'
 
-   
+
 class FeedComponent extends Component {
+
+
     constructor(props) {
         super(props)
-        this.state = {feed:new engine().request_feed()}
+        this.state = {}
+        new engine().request_feed((res) => this.updateFeed(res))
+    }
+    componentDidMount() {
+        // Haha, good luck getting this to work future me. Have to call async functions to load the feed. Consider some sort of callback to update the component when it occurs.
+
+    }
+    updateFeed(feed) {
+        console.log("Feed updated")
+        console.log(feed)
+        const temporarilyReformattedFeed = {
+            "news": [
+              {
+                "category": "Top News",
+                "categoryColor": "#ddd9fc",
+                "categoryId":"1",
+                "articles": feed
+              }
+            ]
+        }
+        console.log(temporarilyReformattedFeed.news[0].articles)
+        this.setState({ feed: temporarilyReformattedFeed })
+        console.log(this.state.feed.news[0].articles)
+        console.log(this.state)
     }
     render() {
-        // const { classes } = this.props;
-        // const classes = useStyles()
         return (
             <div className="FeedRoot">
-                <Grid container>
-                    <Grid item xs={0}  sm={1}/>
-                    <Grid item xs={12} sm={10}>
-                        {this.state.feed.news.map(subject => <SubjectSection key={subject.categoryId}  categoryColor={subject.categoryColor} articles={subject.articles} category={subject.category}/>)}                    
+                {this.state.feed != null ? (
+                    <Grid container>
+                        <Grid item xs={0} sm={1} />
+                        <Grid item xs={12} sm={10}>
+                            {this.state.feed.news.map(subject => <SubjectSection key={subject.categoryId} categoryColor={subject.categoryColor} articles={subject.articles} category={subject.category} />)}
+                        </Grid>
+                        <Grid item xs={0} sm={1} />
                     </Grid>
-                    <Grid item xs={0}  sm={1}/>
-                </Grid>
+                ) : (
+                        <Typography>LOADING</Typography>
+                    )}
             </div>
         );
     }
--- client/src/personalization_engine/engine.js
+++ client/src/personalization_engine/engine.js
@@ -5,7 +5,17 @@ export default class engine {
     //     // Dive into some blockstack stuff later
     //     // Some variables may need storage
     // }
-    request_feed() {
+   async request_feed(callback) {
+        // Temporarily surround with category as the api has not added that functionality yet
+        const response = await fetch('http://localhost:8000/api/recentarticles');
+        const data = await response.json()
+        if (response.ok) {
+
+            callback(data)
+        } else {
+            console.log ("Something bad happend in engine.request_feed().")
+            // callback("Err");
+        }
         return text;
     }
 }
